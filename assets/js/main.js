5bd0de0ed89e924956ceeb7d81d5da6434cf9833
Michael Sarpong <mike.sarp.ky@gmail.com>
2020-02-09 23:52:42 +0000

    a check icon is appended when button is selected

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,19 @@
 let buttons = document.querySelectorAll('button');
 
 for (let button of buttons) {
+    let checked = document.createElement('i');
+    checked.classList.add('fa', 'fa-check');
+
     button.addEventListener('click', function() {
         if (!button.classList.contains('selected')) {
             button.classList.add('selected');
-            let option = button.value;
-            console.log(option);
+            button.appendChild(checked);
+
+            // let option = button.value;
+            // console.log(option);
         } else {
             button.classList.remove('selected');
-
+            button.removeChild(checked);
         }
     })
 }
